9444bea81c7a6c11889354c3adf9296e941ef275
Ayush Kumar <ayushtiwari0803@gmail.com>
2025-05-27 22:58:24 +0530

    into_iter

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
 fn main() {
     let nums = vec![1, 2, 3, 5, 7];
-    let mut nums_iter = nums.iter();
+    let mut nums_iter = nums.into_iter(); // wen need to make nums_iter mutable because we are using .next()which splits an iterator into its element
+    // into_iter() creates an iterator by taking the ownership from the vector but not by borrowing it.
 
     while let Some(val) = nums_iter.next(){
         println!("{}", val);
     }
-    println!("{:?}", nums);
+    // println!("{:?}", nums);
 }
