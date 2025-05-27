5bce53b05d43416bef7ceb4286c054172153eaeb
Ayush Kumar <ayushtiwari0803@gmail.com>
2025-05-27 23:27:59 +0530

    Iterator adaptor: map

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,10 @@ fn main() {
     let nums = vec![1, 2, 3, 5, 7];
     let nums_iter = nums.iter(); 
 
-    let total: i32 = nums_iter.sum(); // A consuming adapter which takes the ownership of an iterator. How do we know that? Since it takes self as a parameter but not &self as a parameter.
+    let mut iter2 = nums_iter.map(|x| x+1); // this is a iterator adaptor
 
-    println!("{}", total);
-    // print!("{:?}", nums_iter); // We can't use the iterator anymore here because it is consumed by the sum adapter
-    println!("{:?}", nums); // We can still us the vector since the adapter consumes the iterator but not the vector
+    while let Some(val) = iter2.next() {
+        println!("{}", val);
+    }
+    println!("{:?}", nums);
 }
