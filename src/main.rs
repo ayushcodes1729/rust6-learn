074b2c47afc16bfd35c35bc979bd37394d2f6b87
Ayush Kumar <ayushtiwari0803@gmail.com>
2025-05-27 17:51:39 +0530

    Mutable iterator"

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,8 @@
 fn main() {
-    let nums = vec![1, 2, 3, 5, 7];
-    let nums_iter = nums.iter(); //nums.iter() let's nums_iter to borrow the values inside nums vector.
+    let mut nums = vec![1, 2, 3, 5, 7];
+    let nums_iter = nums.iter_mut(); //nums.iter() let's nums_iter to borrow the values inside nums vector.
     for values in nums_iter {
-        println!("{}", values);
+        *values = *values + 1
     }
-    // here we can again get nums vector 
     println!("{:?}", nums);
 }
