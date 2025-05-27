b0ae676c95a6f66f94d53d83f090b8d1feaef6b9
Ayush Kumar <ayushtiwari0803@gmail.com>
2025-05-27 17:46:37 +0530

    Iterators

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,9 @@
-use std::collections::{btree_map::Values, HashMap};
-
 fn main() {
-    let pairs = vec![
-        (String::from("Ayush"), 24),
-        (String::from("Harkirat"), 21),
-        (String::from("Ayush"), 21),
-    ];
-
-    let ghp = hp_by_pairs(pairs);
-    println!("Hashmap is {:?}", ghp)
-
-}
-
-fn hp_by_pairs(pairs: Vec<(String, i32)>)-> HashMap<String, i32>{
-    let mut hm = HashMap::new();
-    for (key, val) in pairs {
-        hm.insert(key, val);
+    let nums = vec![1, 2, 3, 5, 7];
+    let nums_iter = nums.iter(); //nums.iter() let's nums_iter to borrow the values inside nums vector.
+    for values in nums_iter {
+        println!("{}", values);
     }
-    return hm;
+    // here we can again get nums vector 
+    println!("{:?}", nums);
 }
-
-// fn main() {
-//     let mut users: HashMap<String, u32> = HashMap::new();
-//     users.insert(String::from("Ayush"), 21);
-//     users.insert(String::from("Harkirat"), 24);
-
-//     let admin = users.get("Ayush");
-//     let age = match admin {
-//         Some(age) => age,
-//         None => {
-//             println!("Enter a valid user");
-//             &0
-//         },
-//     };
-//     if *age > 0 {
-//         println!("User age: {}", age);
-//     }
-// }
--- src/pvs.rs
+++ src/pvs.rs
@@ -1,4 +1,6 @@
 
+
+
 // use std::collections::HashMap;
 
 // fn main() {
