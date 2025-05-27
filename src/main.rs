c77736361f66176557047069ecc98ce85888e7d5
Ayush Kumar <ayushtiwari0803@gmail.com>
2025-05-27 22:49:46 +0530

    iterator using while and iter.next()

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
 fn main() {
-    let mut nums = vec![1, 2, 3, 5, 7];
-    let nums_iter = nums.iter_mut(); //nums.iter() let's nums_iter to borrow the values inside nums vector.
-    for values in nums_iter {
-        *values = *values + 1
+    let nums = vec![1, 2, 3, 5, 7];
+    let mut nums_iter = nums.iter();
+
+    while let Some(val) = nums_iter.next(){
+        println!("{}", val);
     }
     println!("{:?}", nums);
 }
--- src/pvs.rs
+++ src/pvs.rs
@@ -9,6 +9,17 @@
 // }
 
 
+// fn main() {
+//     let nums = vec![1, 2, 3, 5, 7];
+//     let nums_iter = nums.iter(); //nums.iter() let's nums_iter to borrow the values inside nums vector.
+//     for values in nums_iter {
+//         println!("{}", values);
+//     }
+//     // here we can again get nums vector 
+//     println!("{:?}", nums);
+// }
+
+
 
 // use std::collections::HashMap;
 
