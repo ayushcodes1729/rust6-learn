34eaae9d72eb6f32e7601b599cd9cb0757248ae4
Ayush Kumar <ayushtiwari0803@gmail.com>
2025-05-29 00:18:36 +0530

    Longest string with lifetimes

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,19 @@
-// Using lifetimes with structs
-struct User<'a> {
-    name: &'a str
-}
+// longest string with lifetimes
+use std::fmt::Display;
 
-// Why do we need structs with references to have a lifetime parameter? So that we know how long the struct is gonna live. If the borrowed value inside the struct dies some how(i.e. goes out of scope) then the struct should also die, else it will create a dangling pointer error.
 
 fn main() {
-    let first_name = String::from("Ayush");
-    let user = User {name: &first_name};
-    println!("The name of the user is {}", user.name);
+    let s1= String::from("Ayush");
+    let s2= String::from("Harkirat");
+    let res = longest_with_an_annoucement(&s1, &s2, String::from("Hey this is ann"));
+    println!("{}",res)
+}
+
+fn longest_with_an_annoucement<'a, T>(str1: &'a str,str2: &'a str, ann: T)-> &'a str where T: Display, {
+    println!("Announcement! {}",ann);
+    if str1.len() > str2.len() {
+        str1
+    }else {
+        str2
+    }
 }
--- src/pvs.rs
+++ src/pvs.rs
@@ -1,3 +1,16 @@
+// // Using lifetimes with structs
+// struct User<'a> {
+//     name: &'a str
+// }
+
+// // Why do we need structs with references to have a lifetime parameter? So that we know how long the struct is gonna live. If the borrowed value inside the struct dies some how(i.e. goes out of scope) then the struct should also die, else it will create a dangling pointer error.
+
+// fn main() {
+//     let first_name = String::from("Ayush");
+//     let user = User {name: &first_name};
+//     println!("The name of the user is {}", user.name);
+// }
+
 // // Lifetimes
 
 // fn main() {
