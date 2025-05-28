9c112d66f879f528c5373d2d11f547e11776da94
Ayush Kumar <ayushtiwari0803@gmail.com>
2025-05-29 01:10:34 +0530

    Message passing using channels

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,16 @@
-// Using Move closures in rust
-
-use std::thread;
+use std::{sync::mpsc, thread};
+// Message Passing: Channels
 
 fn main() {
-    let v = vec![1,2,3,4,5];
-
-    // The below code doesn't runs because v may go out of scope even before the thread get's spwan and since the thread borrows the vector v it can cause a dangling pointer error that's why we move the ownership of vec v to the macro inside the thread by using move keyword
-    // let handle = thread::spawn(|| {
-    //     println!("{:?}",v)
-    // });
-    let handle = thread::spawn(move || {
-        println!("{:?}",v)
+    let (tx, rx) = mpsc::channel();
+    thread::spawn(move || {
+        let val = String::from("Secret message from thread 1");
+        tx.send(val).unwrap()
     });
 
-    // println!("{:?}", v); // Err: borrow of a moved value v
-
+    let handle = thread::spawn(move || {
+        let recieved = rx.recv().unwrap();
+        println!("Got: {}", recieved)
+    });
     handle.join().unwrap();
 }
--- src/pvs.rs
+++ src/pvs.rs
@@ -1,3 +1,23 @@
+// // Using Move closures in rust
+
+// use std::thread;
+
+// fn main() {
+//     let v = vec![1,2,3,4,5];
+
+//     // The below code doesn't runs because v may go out of scope even before the thread get's spwan and since the thread borrows the vector v it can cause a dangling pointer error that's why we move the ownership of vec v to the macro inside the thread by using move keyword
+//     // let handle = thread::spawn(|| {
+//     //     println!("{:?}",v)
+//     // });
+//     let handle = thread::spawn(move || {
+//         println!("{:?}",v)
+//     });
+
+//     // println!("{:?}", v); // Err: borrow of a moved value v
+
+//     handle.join().unwrap();
+// }
+
 // // Multithreading
 
 // use std::{thread, time::Duration};
