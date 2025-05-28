c0c00e21fd4db2141e8b08c7df9cfbc0697ceec6
Ayush Kumar <ayushtiwari0803@gmail.com>
2025-05-29 01:48:40 +0530

    Large summing using multithreading

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,29 @@
-use std::{sync::mpsc, thread};
-// Message Passing: Channels
+// Sum of all numbers from 1 to 100000000 using multithreading
+
+use std::{
+    sync::mpsc,
+    thread::{self, spawn},
+};
+
 
 fn main() {
     let (tx, rx) = mpsc::channel();
-    thread::spawn(move || {
-        let val = String::from("Secret message from thread 1");
-        tx.send(val).unwrap()
-    });
+    for i in 0..10 {
+        let producer = tx.clone();
+        spawn(move || {
+            let mut ans: i64 = 0;
+            for j in 0..10000000+1 {
+                ans= ans + (i*10000000+j);
+            }
+            producer.send(ans).unwrap();
+        });
+    }
+    drop(tx); // The original tx variable never drops therefore the rx will keep waiting for it, that's why we drop it explicitly
 
-    let handle = thread::spawn(move || {
-        let recieved = rx.recv().unwrap();
-        println!("Got: {}", recieved)
-    });
-    handle.join().unwrap();
+    let mut ans: i64 = 0;
+    for val in rx {
+        println!("Recieved value: {}",val);
+        ans+= val;
+    }
+    println!("Sum of 1 to 100000000 = {}", ans);
 }
--- src/pvs.rs
+++ src/pvs.rs
@@ -1,3 +1,21 @@
+// use std::{sync::mpsc, thread};
+// // Message Passing: Channels
+
+// fn main() {
+//     let (tx, rx) = mpsc::channel();
+//     thread::spawn(move || {
+//         let val = String::from("Secret message from thread 1");
+//         tx.send(val).unwrap()
+//     });
+
+//     let handle = thread::spawn(move || {
+//         let recieved = rx.recv().unwrap();
+//         println!("Got: {}", recieved)
+//     });
+//     handle.join().unwrap();
+// }
+
+
 // // Using Move closures in rust
 
 // use std::thread;
